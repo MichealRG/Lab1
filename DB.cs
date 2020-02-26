452b8e45717fc1d855dc0a0a0afa9ffa44717b52
Michael <krzyzowskim1998@gmail.com>
2020-02-26 12:53:35 +0100

    First version

---
+++ DB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Lab1
+{
+    public class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM Region";
+            var command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]+" "+reader[1]); //albo nazwa kolumny reader["CompanyName"]
+            }
+            reader.Close();
+
+        }
+        public void Insert(SqlConnection connection,int id, string regionName)
+        {
+            var sql = $"INSERT INTO Region(RegionId, RegionDescription) VALUES (@id, '{regionName}')";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            //command.Parameters.AddWithValue("@regionName", regionName);
+            int affected= command.ExecuteNonQuery();
+            Console.WriteLine($"{ affected} rows insterted");
+        }
+        public void Drop(SqlConnection connection, int id)
+        {
+            var sql = " DELETE FROM Region WHERE RegionID = @id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{ affected} row deleted");   
+
+        }
+        public void Update(SqlConnection connection, int id,int newId)
+        {
+            var sql = "UPDATE Region SET RegionId=@newId, RegionDescription='Bielsko-Biala' WHERE RegionId=@id";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@newId", newId);
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{ affected} row updated");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+//prudykath@gmail.com
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionsString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+             //to wyzej w propertiesie BD
+            using var connection = new SqlConnection(connectionsString); //trzyma połączenie po twojej stonie i servera chyba ze jest using wtedy zamyka za klamrą ostatnią
+            var db = new DB();
+            connection.Open(); //otwiera przygotwane połączenie błąd w connections stringu sie tu objawia
+
+            db.Drop(connection,10);
+            db.Drop(connection,11);
+            db.Select(connection);
+            Console.WriteLine();
+            //while (reader.Read())
+            //{
+            //    Console.WriteLine(reader[0]); //albo nazwa kolumny reader["CompanyName"]
+            //}
+            db.Insert(connection,10, "Bielsko");
+            db.Select(connection);
+            Console.WriteLine();
+
+
+            //db.update
+            db.Update(connection,10,11);
+            db.Select(connection);
+            connection.Close(); //zamyka połączenia ale using powinien zamknac
+
+        }
+    }
+}
